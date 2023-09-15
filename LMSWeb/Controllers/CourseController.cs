3c8870bbe6ba50cc720e60efddf0939a04b04573
umgmz56 <umgmz56@gmail.com>
2023-09-15 11:44:57 +0300

    Course list pulled from DB.

--- LMSWeb/Controllers/CourseController.cs
+++ LMSWeb/Controllers/CourseController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LMS.DataAccess.Repository.IRepository;
+using LMS.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LMSWeb.Controllers
 {
     public class CourseController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public CourseController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Course> courseList = _unitOfWork.Course.GetAll();
+            return View(courseList);
         }
     }
 }
