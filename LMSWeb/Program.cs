cbb499f329d2923cdc4b3bc257c0287b55648733
OmerYanci <omer.ync19@gmail.com>
2023-09-19 13:14:09 +0300

    new changes

--- LMSWeb/Program.cs
+++ LMSWeb/Program.cs
@@ -9,9 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlServer(builder.Configuration["AzureDBConnection"])
-);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddTransient<ContentCountViewComponent>();
 builder.Services.AddTransient<CourseCountViewComponent>();
